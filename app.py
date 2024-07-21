a7e55de9923e536241958b461e7bdf794920bb53
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-07-21 18:55:11 +0200

    App update

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+posts = [
+            {
+                "username" : "John Deppo",
+                "title"    : "Hello",
+                "content"  : "This is the first blog post of the day",
+                "time"     : "time"
+            }
+        ]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return "About"
+
+@app.route("/contacts")
+def contacts():
+    return "Contacts"
+
+@app.route("/profile")
+def profile():
+    return "Profile"
+
+if __name__ == "__main__":
+    app.run(debug=True)
